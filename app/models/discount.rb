d509ac14b759f1e702277bb18348434b541c990e
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-07 15:15:18 -0700

    Add validations to discount

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,9 @@
 class Discount < ApplicationRecord
+  validates :percentage_discount, presence: true
+  validates :quantity_threshold, presence: true
+
+  validates :percentage_discount, numericality: { less_than: 1 }
+  validates :quantity_threshold, numericality: true
+
   belongs_to :merchant
 end
