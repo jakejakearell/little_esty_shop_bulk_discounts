689308651cbe6b1862b94347b8a4cd76dc7c34c2
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-07 15:58:09 -0700

    Add create action and sad path options

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -10,4 +10,23 @@ class DiscountsController < ApplicationController
 
   def new
   end
+
+  def create
+    discount = Discount.new(discount_params)
+    if discount.save
+      redirect_to merchant_discounts_path(params[:merchant_id])
+    elsif discount.percentage_discount_exists? && discount.percentage_discount_greater_than_one?
+      flash[:notice] = "Discount not created: Percent discount must be entered as a decimal."
+      render :new
+    else
+      flash[:notice] = "Discount not created: Required information missing or invalid."
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:quantity_threshold, :percentage_discount, :merchant_id)
+  end
 end
