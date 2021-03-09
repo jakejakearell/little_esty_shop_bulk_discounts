e37400f5b2aa6a67f8b0595faf4ea74800380c33
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-09 13:15:29 -0700

    Uncomment out code

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Invoice, type: :model do
   before :each do
     @merchant1 = Merchant.create!(name: 'Hair Care')
 
-    # @discount_1 = @merchant1.discounts.create!(quantity_threshold: 5, percentage_discount: 0.5 )
-    # @discount_2 = @merchant1.discounts.create!(quantity_threshold: 9, percentage_discount: 0.6 )
+    @discount_1 = @merchant1.discounts.create!(quantity_threshold: 5, percentage_discount: 0.5 )
+    @discount_2 = @merchant1.discounts.create!(quantity_threshold: 9, percentage_discount: 0.6 )
 
     @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
     @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
