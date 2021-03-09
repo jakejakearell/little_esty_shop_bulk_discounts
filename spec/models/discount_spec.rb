b21d49ba9b278e8ee05989cf2ea02de84f02fddb
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-09 13:15:12 -0700

    Add relationship tests

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -12,6 +12,9 @@ RSpec.describe Discount, type: :model do
 
   describe "relationships" do
     it { should belong_to :merchant }
+    it { should have_many(:items).through(:merchant) }
+    it { should have_many(:invoice_items).through(:items) }
+    it { should have_many(:invoices).through(:invoice_items) }
   end
 
   describe "validations" do
